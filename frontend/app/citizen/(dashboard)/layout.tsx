ad8f821ae64d6f1c7638208503ff429cb8f36734
swasti <jainsanjna336@gmail.com>
2026-09-01 23:58:41 +0530

    duplicate files removed from citizen

--- frontend/app/citizen/(dashboard)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import AppShell from "../../../components-citizen/layout/AppShell";
-
-export default function DashboardLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <AppShell role="citizen">{children}</AppShell>;
-}
