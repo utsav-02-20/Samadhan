492cbf91f537140acd2de7aa0feb0cba7f664d62
UTSAV KUMAR <z81600707@gmail.com>
2026-09-02 13:29:56 +0530

    Create New Department Page
    Added a new page for creating departments with a form.

---
+++ frontend/app/department/departments/new/page.js
@@ -0,0 +1,19 @@
+/*
+|--------------------------------------------------------------------------
+| Page: Create Department
+|--------------------------------------------------------------------------
+| Purpose:
+| - Department creation form.
+| - Calls POST /api/departments.
+|--------------------------------------------------------------------------
+*/
+
+"use client";
+
+export default function NewDepartmentPage() {
+  return (
+    <div>
+      <h1>Create Department</h1>
+    </div>
+  );
+}
